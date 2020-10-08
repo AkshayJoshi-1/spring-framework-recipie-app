f871c2323575e488d625b22f64d33c5773094513
David Webb <error1209@hotmail.com>
2020-10-08 18:34:42 +0530

    - Image persistence and retrieval

--- src/main/java/com/spring/framework/recipeapp/controller/ImageController.java
+++ src/main/java/com/spring/framework/recipeapp/controller/ImageController.java
@@ -1,13 +1,20 @@
 package com.spring.framework.recipeapp.controller;
 
+import com.spring.framework.recipeapp.command.RecipeCommand;
 import com.spring.framework.recipeapp.service.ImageService;
 import com.spring.framework.recipeapp.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 @Slf4j
@@ -35,6 +42,26 @@ public class ImageController {
     public String handleImagePost(@PathVariable String recipeId,
                                   @RequestParam("imageFile") MultipartFile imageFile) {
         imageService.saveImageFile(Long.valueOf(recipeId), imageFile);
-        return "redirect:/recipe" + recipeId + "/show";
+        return "redirect:/recipe/" + recipeId + "/show";
+    }
+
+    @GetMapping("getImage")
+    public void renderImage(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+
+        if(recipeCommand == null || recipeCommand.getImage() == null) {
+            return;
+        }
+
+        byte[] imageBytes = new byte[recipeCommand.getImage().length];
+
+        for(int i = 0; i < imageBytes.length; i++) {
+            imageBytes[i] = recipeCommand.getImage()[i];
+        }
+
+        response.setContentType("image/jepg");
+        InputStream imageStream = new ByteArrayInputStream(imageBytes);
+
+        IOUtils.copy(imageStream, response.getOutputStream());
     }
 }
--- src/main/java/com/spring/framework/recipeapp/service/impl/ImageServiceImpl.java
+++ src/main/java/com/spring/framework/recipeapp/service/impl/ImageServiceImpl.java
@@ -1,16 +1,53 @@
 package com.spring.framework.recipeapp.service.impl;
 
+import com.spring.framework.recipeapp.domain.Recipe;
+import com.spring.framework.recipeapp.repository.RecipeRepository;
 import com.spring.framework.recipeapp.service.ImageService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Service
 @Slf4j
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(@Autowired RecipeRepository recipeService) {
+        this.recipeRepository = recipeService;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+
+        if(recipeOptional.isEmpty()) {
+            return;
+        }
+
+        Recipe recipe = recipeOptional.get();
+
+        try {
+            Byte[] imageBytes = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for(byte imageByte : file.getBytes()) {
+                imageBytes[i] = imageByte;
+                i++;
+            }
+
+            recipe.setImage(imageBytes);
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
